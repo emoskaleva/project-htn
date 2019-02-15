a08d52dc8365c8aff03293899dbca6a495481d17
emoskaleva <47529635+emoskaleva@users.noreply.github.com>
2019-02-15 16:27:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import parser as d_p
 import problem as p_p
+import grounding aa g
 import sys
 
 def main(argv):
-    d_p.parse(argv[1])
-    p_p.problem_parser(argv[2])
+    d_p.parse(argv[0])
+    p_p.problem_parser(argv[1])
+    g.groundind(argv[0], argv[1])
 
 if __name__ == '__main__':
     main(sys.argv[1:])
