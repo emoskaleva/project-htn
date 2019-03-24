a2282eb82f62bdac09eb72477b211f1729ab459b
emoskaleva <47529635+emoskaleva@users.noreply.github.com>
2019-03-25 02:07:32 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def main(argv):
     #d_p.parser(argv[0])
     #p_p.problem_parser(argv[1])
     #g.grounding(argv[0], argv[1])
-    planner.function(argv[0], argv[1])
+    plan = planner.HTN_planner()
+    plan.function(argv[0], argv[1], argv[2])
 
 if __name__ == '__main__':
     main(sys.argv[1:])
